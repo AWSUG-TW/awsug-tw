05bd73648cdb2db398e458e2199c644e6520d638
Sammy Lin <sammylintw@gmail.com>
2013-05-13 21:23:36 +0800

    fix assets sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,6 +1,6 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.fog_directory = 'cdn1.awsug.tw/assets'
+  config.fog_directory = 'assets'
   config.aws_access_key_id = Setting.s3_access_key_id
   config.aws_secret_access_key = Setting.s3_secret_access_key
   config.fog_region = Setting.s3_fog_region
