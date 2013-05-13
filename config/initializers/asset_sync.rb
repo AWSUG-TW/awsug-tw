e2ae69bbd6816ccda02c506d1f7285c01401b1a7
Sammy Lin <sammylintw@gmail.com>
2013-05-13 21:57:44 +0800

    fix

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,4 +4,5 @@ AssetSync.configure do |config|
   config.aws_access_key_id = Setting.s3_access_key_id
   config.aws_secret_access_key = Setting.s3_secret_access_key
   config.fog_region = Setting.s3_fog_region
+  config.fail_silently = true
 end
